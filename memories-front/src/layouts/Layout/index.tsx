25c964956b7135b2aa0ddbd19c97ada34261ed49
surimgit <ruriaim@gmail.com>
2025-03-11 16:32:52 +0900

    2025-03-11

--- memories-front/src/layouts/Layout/index.tsx
+++ memories-front/src/layouts/Layout/index.tsx
@@ -1,12 +1,67 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React, { MouseEvent, useEffect, useRef, useState } from 'react'
+import { Outlet, useLocation } from 'react-router'
 
 import './style.css';
+import { env } from 'process';
 
+// component: 공통 레이아웃 컴포넌트 //
 export default function Layout() {
+
+// state: 경로 상태
+  const { pathname } = useLocation();
+
+// state: My Content List 요소 참조 //
+const myContentListRef = useRef<HTMLDivElement | null>(null);
+
+// state: My Content 드롭다운 상태 //
+const [showMyContent, setShowMyContent] = useState<boolean>(false);
+
+// variable: 기억력 검사 클래스 //
+const memoryTestClass = pathname.startsWith('/memory-test') ? 'navigation-item active' : 'navigation-item'
+// variable: 집중력 검사 클래스 //
+const concentrationTestClass = pathname.startsWith('/concentration-test') ? 'navigation-item active' : 'navigation-item'
+
+// event handler: My Content 클릭 이벤트 처리 //
+const onMyContentClickHandler = () => {
+  setShowMyContent(!showMyContent);
+};
+
+// effect: My Content 드롭다운 상태가 변경될 시 실행할 함수 //
+useEffect(() => {
+  const onOutSideClickHandler = (event: any) => {
+    if(myContentListRef.current && !myContentListRef.current.contains(event.target as Node)){
+      setShowMyContent(false);
+    }
+  };
+
+  if(!showMyContent) return;
+
+  document.addEventListener('mousedown', onOutSideClickHandler)
+}, [showMyContent]);
+
+// render: 공통 레이아웃 컴포넌트 렌더링 //
   return (
-    <div>
-      <Outlet/>
+    <div id="layout-wrapper">
+      <div id='top-bar'>
+        <div className='navigation'>
+          <div className='title'>Memories</div>
+          <div className='navigation-list'>
+            <div className={memoryTestClass}>기억력 검사</div>
+            <div className={concentrationTestClass}>집중력 검사</div>
+          </div>
+        </div>
+        <div className='my-content' onClick={onMyContentClickHandler}>
+          {showMyContent && 
+          <div ref={myContentListRef} className='my-content-list'>
+            <div className='my-content-item'>일기</div>
+            <div className='my-content-item'>로그아웃</div>
+          </div>
+          } 
+        </div>
+      </div>
+      <div id='main'>
+        <Outlet/>
+      </div>
     </div>
   )
 }
--- memories-front/src/views/Auth/SignUp/index.tsx
+++ memories-front/src/views/Auth/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent, useState } from 'react'
-import './style.css'
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import './style.css';
 import { AuthPage } from 'src/types/aliases';
 import InputBox from 'src/components/InputBox';
+import {Address, useDaumPostcodePopup} from 'react-daum-postcode';
 
 // interface: 회원가입 컴포넌트 속성 //
 interface Props{
@@ -24,7 +25,7 @@ export default function SingUp(props: Props) {
 // state: 사용자 주소 상태 //
   const [userAddress, setUserAddress] = useState<string>('');
 // state: 사용자 상세 주소 상태 //
-  const [userDetailaddress, setUserDetailAddress] = useState<string>('');
+  const [userDetailAddress, setUserDetailAddress] = useState<string>('');
 
 // state: 사용자 이름 메세지 상태 //
   const [userNameMessage, setUserNameMessage] = useState<string>('');
@@ -36,29 +37,68 @@ const [userPasswordMessage, setUserPasswordMessage] = useState<string>('');
 const [userPasswordCheckMessage, setUserPasswordCheckMessage] = useState<string>('');
 // state: 사용자 주소 메세지 상태 //
 const [userAddressMessage, setUserAddressMessage] = useState<string>('');
-
 // state: 사용자 아이디 메시지 에러 상태 //
 const [userIdMessageError, setUserIdMessageError] = useState<boolean>(false);
+// state: 사용자 중복 확인 상태 //
+const [isUserIdChecked, setUserIdChecked] = useState<boolean>(false);
+// state: 사용자 비밀번호 패턴 일치 상태 //
+const [isUserPasswordChecked, setUserPasswordChecked] = useState<boolean>(false);
+// state: 사용자 비밀번호 동일 여부 상태 //
+const [isUserPasswordEqual, setUserPasswordEqual] = useState<boolean>(false);
+
+
+// variable: 중복 버튼 활성화 //
+const isUserIdCheckButtonActive = userId !== '';
+// variable: 회원가입 버튼 활성화 //
+const issignUpButtonActive = 
+  userName && userId && userPassword && userPasswordCheck && userAddress &&
+  isUserIdChecked && isUserPasswordChecked && isUserPasswordEqual;
+// variable: 회원가입 버튼 클래스 //
+const issignUpButtonClass = `button ${issignUpButtonActive ? 'primary' : 'disable'} fullwidth`; 
+
+
+// function: 다음 포스트 코드 팝업 오픈 함수 //
+const open = useDaumPostcodePopup();
+
+// function: 다음 포스트 코드 완료 처리 함수 //
+const daumPostCompleteHandler = (data:Address) => {
+  const { address } = data;
+  setUserAddress(address);
+  setUserAddressMessage('');
+};
+
 
 // event handler: 사용자 이름 변경 이벤트 처리 //
 const onUserNameChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
   const { value } = event.target;
   setUserName(value);
+
+  setUserNameMessage('');
 }
 // event handler: 사용자 아이디 변경 이벤트 처리 //
 const onUserIdChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
   const { value } = event.target;
   setUserId(value);
+
+  setUserIdChecked(false);
+  setUserIdMessage('');
+  setUserIdMessageError(false);
 }
 // event handler: 사용자 비밀번호 변경 이벤트 처리 //
 const onUserPasswordChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
   const { value } = event.target;
   setUserPassword(value);
+  const regexp = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,13}$/;
+  const isMatch = regexp.test(value);
+  const message = isMatch ? '' : '영문, 숫자를 혼용하여 8 ~ 13자 입력해주세요';
+  setUserPasswordMessage(message);
+  setUserPasswordChecked(isMatch);
 }
 // event handler: 사용자 비밀번호 확인 변경 이벤트 처리 //
 const onUserPasswordCheckChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
   const { value } = event.target;
-  setUserPasswordCheck(value);
+  setUserPasswordCheck(value);  
+  
 }
 // event handler: 사용자 주소 변경 이벤트 처리 //
 const onUserAddressChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
@@ -70,55 +110,75 @@ const onUserDetailAddressChangeHandler = (event:ChangeEvent<HTMLInputElement>) =
   const { value } = event.target;
   setUserDetailAddress(value);
 }
-
 // event handler: 중복 확인 버튼 클릭 이벤트 처리 //
 const onCheckUserIdClickHandler = () => {
+  if(!isUserIdCheckButtonActive) return;
 
+  const idList = ['qwer1234', 'rewq4321', 'abcd1234'];
+  const isExist = idList.includes(userId);
+  const message = isExist ? '이미 사용중인 아이디입니다' : '사용가능한 아이디입니다';
+  setUserIdMessage(message);
+  setUserIdMessageError(isExist);
+  setUserIdChecked(!isExist);
 };
-
 // event handler: 주소 검색 버튼 클릭 이벤트 처리 //
 const onSearchAddressClickHandler = () => {
+  open({onComplete: daumPostCompleteHandler})
+};
+// event handler: 회원가입 버튼 클릭 이벤트 처리 //
+const onSignUpClickHandler = () => {
+  if(!userName) setUserNameMessage('이름을 입력해주세요');
+  if(!userPassword) setUserPasswordMessage('비밀번호를 입력해주세요');
+  if(!userAddress) setUserAddressMessage('주소를 입력해주세요');
+  if(!isUserIdChecked) {
+    setUserIdMessage('아이디 중복 확인해주세요');
+    setUserIdMessageError(true);
+  }
+  if(!issignUpButtonActive) return;
 
+  alert('회원가입!');
 };
 
-// variable: 중복 버튼 활성화 //
-const isUserIdCheckButtonActive = userId !== '';
+
+// effect: 사용자 비밀번호 또는 사용자 비밀번호 확인이 변경될 시 실행할 함수 //
+useEffect(() => {
+  const isMatch = userPasswordCheck === userPassword;
+  const message = isMatch ? '' : '비밀번호가 일치하지 않습니다.';
+  setUserPasswordCheckMessage(message);
+  setUserPasswordEqual(isMatch);
+}, [userPassword, userPasswordCheck])
+
+
 
 // render: 회원가입 컴포넌트 렌더링 //
   return (
-    <div id='auth-sign-up-contanier'>
+    <div id='auth-sign-up-container'>
       <div className='header'>Memories</div>
       <div className='sns-container'>
         <div className='sns-header'>SNS 회원가입</div>
         <div className='sns-button-box'>
-          <div className='sns-button-kakao'></div>
-          <div className='sns-button-naver'></div>
+          <div className='sns-button kakao'></div>
+          <div className='sns-button naver'></div>
         </div>
       </div>
       <div className='divider'></div>
       <div className='input-container'>
-        <InputBox label={'이름'} type='text' value={userName} placeholder={'이름을 입력해주세요.'}
-        onChange={onUserNameChangeHandler} message={userNameMessage} isErrorMessage/>
 
-        <InputBox label={'아이디'} type='text' value={userId} placeholder={'아이디를 입력해주세요.'}
-        onChange={onUserIdChangeHandler} message={userIdMessage} 
-        buttonName={'중복 확인'} onButtonClick={onCheckUserIdClickHandler} isButtonActive={isUserIdCheckButtonActive}/>
+        <InputBox label={'이름'} type={'text'} value={userName} placeholder={'이름을 입력해주세요.'} onChange={onUserNameChangeHandler} message={userNameMessage} isErrorMessage />
 
-        <InputBox label={'비밀번호'} type='password' value={userPassword} placeholder={'비밀번호를 입력해주세요.'}
-        onChange={onUserPasswordChangeHandler} message={userPasswordMessage} isErrorMessage/>
+        <InputBox label={'아이디'} type={'text'} value={userId} placeholder={'아이디를 입력해주세요.'} onChange={onUserIdChangeHandler} message={userIdMessage} buttonName={'중복 확인'} onButtonClick={onCheckUserIdClickHandler} isErrorMessage={userIdMessageError} isButtonActive={isUserIdCheckButtonActive} />
 
-        <InputBox label={'비밀번호 확인'} type='password' value={userPasswordCheck} placeholder={'비밀번호를 입력해주세요.'}
-        onChange={onUserPasswordCheckChangeHandler} message={userPasswordCheckMessage} isErrorMessage/>
+        <InputBox label={'비밀번호'} type={'password'} value={userPassword} placeholder={'비밀번호를 입력해주세요.'} onChange={onUserPasswordChangeHandler} message={userPasswordMessage} isErrorMessage />
 
-        <InputBox label={'주소'} type='text' value={userAddress} placeholder={'주소를 입력해주세요.'}
-        onChange={onUserAddressChangeHandler} message={userAddressMessage} 
-        buttonName={'주소 검색'} onButtonClick={onSearchAddressClickHandler} isErrorMessage isButtonActive readOnly/>
+        <InputBox label={'비밀번호 확인'} type={'password'} value={userPasswordCheck} placeholder={'비밀번호를 입력해주세요.'} onChange={onUserPasswordCheckChangeHandler} message={userPasswordCheckMessage} isErrorMessage />
 
-        <InputBox label={'상세 주소'} type='text' value={userDetailaddress} placeholder={'상세주소를 입력해주세요.'}
-        onChange={onUserDetailAddressChangeHandler} message={''}/>
+        <InputBox label={'주소'} type={'text'} value={userAddress} placeholder={'주소를 입력해주세요.'} onChange={onUserAddressChangeHandler} message={userAddressMessage} buttonName={'주소 검색'} onButtonClick={onSearchAddressClickHandler} isErrorMessage isButtonActive readOnly />
+
+        <InputBox label={'상세 주소'} type={'text'} value={userDetailAddress} placeholder={'상세 주소를 입력해주세요.'} onChange={onUserDetailAddressChangeHandler} message={''} />
+
       </div>
       <div className='button-container'>
-        <div className='button disable fullwidth'>회원가입</div>
+        <div className={issignUpButtonClass} onClick={onSignUpClickHandler}>회원가입</div>
         <div className='link' onClick={() => onPageChange('sign-in')}>로그인</div>
       </div>
     </div>
